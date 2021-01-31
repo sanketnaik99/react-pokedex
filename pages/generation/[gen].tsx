0073be01dad34a3bfc7a0da1a63da1c18aa7d535
Sanket Naik <sanketnaik99@gmail.com>
2021-01-31 22:35:20 +0530

    Added images for documentation.

--- pages/generation/[gen].tsx
+++ pages/generation/[gen].tsx
@@ -163,7 +163,7 @@ const PokemonGeneration: React.FC<Props> = ({
           </div>
         )}
         {isLoading ? (
-          <div className="sm:mt-8 mt-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 grid-cols-1 gap-y-4 md:gap-y-8 gap-x-8">
+          <div className="sm:mt-8 mx-2 sm:mx-auto mt-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 grid-cols-1 gap-y-4 md:gap-y-8 gap-x-8">
             <LoadingCard />
             <LoadingCard />
             <LoadingCard />
