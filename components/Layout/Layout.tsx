c3c979bcec59100890aa50eab2d46b618551be77
Sanket Naik <sanketnaik99@gmail.com>
2021-02-01 15:54:48 +0530

    PWA Fix ✨

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
@@ -32,6 +33,34 @@ const Layout: React.FC = ({ children }) => {
         "w-full flex flex-col",
       ].join(" ")}
     >
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="A Pokedex App created using Next.js (A ReactJS Framework), Typescript, and TailwindCSS."
+        />
+        <title>React PokeDex</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Navbar
         handleToggle={() => {
           currentTheme === "dark" ? setTheme("light") : setTheme("dark");
