7709e207836a3c9c7da07eab9bad820e086641ed
Sanket Naik <sanketnaik99@gmail.com>
2021-02-01 16:00:09 +0530

    user-scalable="no" parameter removed.

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -36,10 +36,7 @@ const Layout: React.FC = ({ children }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta
           name="description"
           content="A Pokedex App created using Next.js (A ReactJS Framework), Typescript, and TailwindCSS."
